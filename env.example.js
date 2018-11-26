297adba2817827605b583f36b2784c242c4274f8
Carlos Duran <carlosduran.xyz@gmail.com>
2018-11-25 20:12:42 -0500

    Update command

--- env.example.js
+++ env.example.js
@@ -1,7 +1,4 @@
 Object.assign(process.env, {
   'APP_SECRET': 'secret-hash',
-  'MONGODB_URI': 'mongodb://localhost:27017/exchange',
-
-  // Sendgrid
-  'SENDGRID_API_KEY': ''
+  'MONGODB_URI': 'mongodb://localhost:27017/exchange'
 })
